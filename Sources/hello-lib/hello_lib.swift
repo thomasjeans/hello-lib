997cd658020b9f83fba57f9e7438705c489c7d58
Thomas Jeans <thomasjeans@outlook.com>
2019-08-20 21:28:57 -0400

    update tests

--- Sources/hello-lib/hello_lib.swift
+++ Sources/hello-lib/hello_lib.swift
@@ -1,5 +1,8 @@
 class Hello {
+    
     static let shared = Hello()
+    
+    var text = "hello"
     
     func sayHello() {
         print(text)
--- Tests/hello-libTests/hello_libTests.swift
+++ Tests/hello-libTests/hello_libTests.swift
@@ -2,11 +2,12 @@ import XCTest
 @testable import hello_lib
 
 final class hello_libTests: XCTestCase {
+    
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(hello_lib().text, "Hello, World!")
+        XCTAssertEqual(Hello.shared.text, "hello")
     }
 
     static var allTests = [
